1bcaca5ff91de17873f72b4cc77b8931ad88b7ea
Kwankyu Ko <kwankyu.kko@gmail.com>
2019-02-16 13:51:16 -0700

    creative2

--- script/script.js
+++ script/script.js
@@ -3,45 +3,13 @@ $(document).ready(function() {
 
 
     // Create a Stripe client.
-    var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+    var stripe = Stripe('pk_test_JovguOj6X5dcza68c2DUaURD');
 
     // Create an instance of Elements.
     var elements = stripe.elements();
 
-    // Custom styling can be passed to options when creating an Element.
-    // (Note that this demo uses a wider set of styles than the guide below.)
-    var style = {
-    base: {
-        color: '#32325d',
-        lineHeight: '18px',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-        color: '#aab7c4'
-        }
-    },
-    invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a'
-    }
-    };
-
     // Create an instance of the card Element.
     var card = elements.create('card', {style: style});
-
-    // Add an instance of the card Element into the `card-element` <div>.
-    card.mount('#card-element');
-
-    // Handle real-time validation errors from the card Element.
-    card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-        displayError.textContent = event.error.message;
-    } else {
-        displayError.textContent = '';
-    }
-    });
 
     // Handle form submission.
     var form = document.getElementById('payment-form');
